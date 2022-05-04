b6d53933862230a43ea501cf4e80327b664bcce9
Ulukbek-Toychuev <t.ulukbek01@gmail.com>
2022-05-04 21:59:21 +0300

    Update by Victoria Komarnitckaia

--- AdditSubtrac.go
+++ AdditSubtrac.go
@@ -2,58 +2,71 @@ package Calculator
 
 import "fmt"
 
+//This function receives the arithmetic operation symbol
 func plusOrMinus(symbol string) string {
-
-	fmt.Println("Введите +/-")
+	fmt.Println("Введите '+', либо, '-' , либо '*', либо '/' ")
 
 	_, err := fmt.Scan(&symbol)
 
 	if err != nil {
 		fmt.Println("Неправильный формат ввода!")
-	} else if symbol != "+" && symbol != "-" {
+	} else if symbol != "+" && symbol != "-" && symbol != "*" && symbol != "/" {
 		fmt.Println("Неправильный формат ввода!")
 		return plusOrMinus(symbol)
 	}
-
 	return symbol
 }
 
-func AddOrSubtract() float64{
+//This function receives the first number
+func getNumber1(number float64) float64 {
+	fmt.Println("Введите первое число")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		fmt.Println("Неправильный формат ввода! Попробуйте еще раз!")
+		return getNumber1(number)
+	}
+	return number
+}
+
+//This function receives the second number
+func getNumber2(number float64) float64 {
+	fmt.Println("Введите второе число")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		fmt.Println("Неправильный формат ввода! Попробуйте еще раз!")
+		return getNumber2(number)
+	}
+	return number
+}
+
+func AddOrSubtract() float64 {
 	var (
 		v1, v2, result float64
-		symbol string
+		symbol         string
 	)
 
-	//Этот кусок кода принимает первое значение
-	fmt.Println("Введите первое число")
-	_, err := fmt.Scan(&v1)
-	if err != nil {
-		fmt.Println("Неправильный формат ввода!")
-		return AddOrSubtract()
-	}
+	v1 = getNumber1(v1)
 
-	//Вводите или + или -
 	symbol = plusOrMinus(symbol)
 
-	//Этот кусок кода принимает первое значение
-	fmt.Println("Введите второе число")
-	_, err = fmt.Scan(&v2)
-	if err != nil {
-		fmt.Println("Неправильный формат ввода!")
-		return AddOrSubtract()
-	}
+	v2 = getNumber2(v2)
 
-	//В зависимости от выбранного символа, два значения или складываются или отнимаются
-	if symbol == "+" {
+	switch symbol {
+	case "+":
 		result = v1 + v2
-	}else if symbol == "-"{
+	case "-":
 		result = v1 - v2
+	case "*":
+		result = v1 * v2
+	case "/":
+		if v2 == 0 {
+			fmt.Println("Деление на 0 запрещено !!!")
+			return getNumber2(v2)
+		}
+		result = v1 / v2
 	}
 
 	fmt.Println(v1, symbol, v2, "=", result)
 
 	return result
 }
-
-
-
